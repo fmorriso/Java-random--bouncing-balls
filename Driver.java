12bdb64da0783860c9b565c69eeae82da2379eb9
Frederick Morrison <frederick.morrison@outlook.com>
2024-07-11 13:01:11 -0600

    finish current GitHub work

--- Driver.java
+++ Driver.java
@@ -4,54 +4,55 @@ import java.awt.Toolkit;
 //
 import javax.swing.JFrame;
 
-public class Driver {
+public class Driver 
+{
 
-    public static void main(String[] args) {
-	// capture size of screen we're using
-	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-
-	// Define the size of the JFrame as a square that is a percentage of the
-	// available screen area and a multiple of 100
-	int frameHeight = (int) (screenSize.height * 75.00 / 100) / 100 * 100;
-	int frameWidth = frameHeight;
-
-	Dimension gameSize = new Dimension(frameWidth, frameHeight);
-	System.out.format("frameSize=%s%n", gameSize);
-
-	JFrame frame = new MainFrame("Random Moving Balls");
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-	// note the use of setPreferredSize instead of setSize, coupled with
-	// frame.pack() below:
-	frame.setPreferredSize(gameSize);
-	frame.setSize(gameSize);
-
-	GameController controller = new GameController(frame);
-
-	MainPanel pnl = new MainPanel(controller);
-	pnl.setSize(gameSize);
-	pnl.setPreferredSize(gameSize);
-
-	// IMPORTANT: without this, the JPanel is not the exact same size as its parent
-	// JFrame
-	// Border zeroBorder = BorderFactory.createEmptyBorder(0, 0, 0, 0);
-	// pnl.setBorder(zeroBorder);
-
-	frame.setLayout(null);
-	frame.getContentPane().add(pnl);
-
-	frame.pack();
-
-	Insets insets = frame.getInsets();
-	System.out.format("Frame insets: %s%n", insets);
-
-	// put the JFrame in the middle of the physical screen
-	frame.setLocationRelativeTo(null);
-	frame.setResizable(false);
-	frame.setVisible(true);
-
-	//System.out.format("Driver: frame: w = %d, h = %d%n", frame.getWidth(), frame.getHeight());
-
-    }
+   public static void main(String[] args) {
+   // capture size of screen we're using
+      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+   
+   // Define the size of the JFrame as a rectangle that is a percentage of the
+   // available screen area and a multiple of 100
+      int frameHeight = (int) (screenSize.height * 85.00 / 100) / 100 * 100;
+      int frameWidth = (int) ( frameHeight * 1.25 );
+   
+      Dimension gameSize = new Dimension(frameWidth, frameHeight);
+      System.out.format("gameSize=%s%n", gameSize);
+   
+      JFrame frame = new MainFrame("Random Moving Balls");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   
+   // note the use of setPreferredSize instead of setSize, coupled with
+   // frame.pack() below:
+      frame.setPreferredSize(gameSize);
+      frame.setSize(gameSize);
+   
+      GameController controller = new GameController(frame);
+   
+      MainPanel pnl = new MainPanel(controller);
+      pnl.setSize(gameSize);
+      pnl.setPreferredSize(gameSize);
+   
+   // IMPORTANT: without this, the JPanel is not the exact same size as its parent
+   // JFrame
+   // Border zeroBorder = BorderFactory.createEmptyBorder(0, 0, 0, 0);
+   // pnl.setBorder(zeroBorder);
+   
+      frame.setLayout(null);
+      frame.getContentPane().add(pnl);
+   
+      frame.pack();
+   
+      Insets insets = frame.getInsets();
+      System.out.format("Frame insets: %s%n", insets);
+   
+   // put the JFrame in the middle of the physical screen
+      frame.setLocationRelativeTo(null);
+      frame.setResizable(false);
+      frame.setVisible(true);
+   
+   //System.out.format("Driver: frame: w = %d, h = %d%n", frame.getWidth(), frame.getHeight());
+   
+   }
 
 }
--- GameController.java
+++ GameController.java
@@ -8,130 +8,131 @@ import javax.swing.Timer;
 
 public class GameController {
 
-    private JFrame parentComponent;
-    private Dimension gameSize;
-    private Insets insets;
-    private ArrayList<RandomBall> balls;
+   private JFrame parentComponent;
+   private Dimension gameSize;
+   private Insets insets;
+   private ArrayList<RandomBall> balls;
 
-    public GameController(JFrame frame) {
-	this.parentComponent = frame;
-	this.gameSize = parentComponent.getSize();
-	this.insets = parentComponent.getInsets();
+   public GameController(JFrame frame) {
+      this.parentComponent = frame;
+      this.gameSize = parentComponent.getSize();
+      this.insets = parentComponent.getInsets();
+   
+      balls = generateBalls();
+      startBallTimers();
+   }
 
-	balls = generateBalls();
-	startBallTimers();
-    }
+   private void startBallTimers() {
+      for (RandomBall b : balls) {
+         b.getTimer().start();
+      }
+   }
 
-    private void startBallTimers() {
-	for (RandomBall b : balls) {
-	    b.getTimer().start();
-	}
-    }
+   private ArrayList<RandomBall> generateBalls() {
+      ArrayList<RandomBall> list = new ArrayList<RandomBall>();
+      int n = getRandomNumberOfBalls();
+      System.out.format("Number of balls to create is %d%n", n);
+      for (int i = 0; i < n; i++) {
+         Color c = getUniqueColor(list);
+         RandomBall b = new RandomBall(getRandomX(), getRandomY(), getRandomDiameter(), c, gameSize, insets);
+         int delay = getRandomDelay();
+         Timer t = new Timer(delay, (
+            ae -> {
+               b.move(this.gameSize.width, this.gameSize.height);
+            }));
+         b.setTimer(t);
+      
+         list.add(b);
+      }
+      return list;
+   }
 
-    private ArrayList<RandomBall> generateBalls() {
-	ArrayList<RandomBall> list = new ArrayList<RandomBall>();
-	int n = getRandomNumberOfBalls();
-	System.out.format("Number of balls to create is %d%n", n);
-	for (int i = 0; i < n; i++) {
-	    Color c = getUniqueColor(list);
-	    RandomBall b = new RandomBall(getRandomX(), getRandomY(), getRandomDiameter(), c, gameSize, insets);
-	    int delay = getRandomDelay();
-	    Timer t = new Timer(delay, (ae -> {
-		b.move(this.gameSize.width, this.gameSize.height);
-	    }));
-	    b.setTimer(t);
+   private Color getUniqueColor(ArrayList<RandomBall> list) {
+   
+   // avoid unnecessary overhead when the list is small/empty
+      if (list.size() <= 1) {
+         return ColorExtensions.getRandomDarkColor();
+      }
+   
+      boolean keepLooking = true;
+   // vary the minimum separation between colors of each ball colors based on the size of the list.
+   // The more items in the list, the less unique the colors need to be.
+      int byTotalRGB = (int) (64.0 / list.size());
+   //System.out.format("uniqueColor: list size = %d, minimum difference = %d%n", list.size(), byTotalRGB);
+      Color c = null;
+      int count = 0;
+      int attempts = 0;
+   // vary the maximum number of attempts to insure unique colors based on the size of the list.
+   // The more items in the list, the less unique the colors need to be.
+      int maxAttempts = (int) (2048.0 / list.size());
+      do {
+         attempts++;
+         c = ColorExtensions.getRandomDarkColor();
+       // check the other balls to see if this new ball's color is unique enough
+         for (RandomBall b : list) {		
+         // System.out.format("Color difference %d = %d%n", i,
+         // ColorExtensions.getColorDifference(c, b.getColor()));
+            if (ColorExtensions.areSignificantlyDifferentColors(c, b.getColor(), byTotalRGB)) {
+               count++;
+            }
+         }
+       // stop if we have unique colors or have exceeded the maximum number of attempts to find a unique color
+         if (count == list.size()) {
+            keepLooking = false;
+         } else if (attempts == maxAttempts) {
+            System.out.format("gave up looking for unique color in list of size = %d after %d attempts%n", list.size(), attempts);
+            keepLooking = false;
+         }
+      
+      } while (keepLooking);
+   
+      return c;
+   
+   }
 
-	    list.add(b);
-	}
-	return list;
-    }
+   private int getRandomNumberOfBalls() {
+      final int MAX = 9;
+      final int MIN = 3;
+      final int RANGE = MAX - MIN + 1;
+      return (int) (Math.random() * RANGE + MIN);
+   }
 
-    private Color getUniqueColor(ArrayList<RandomBall> list) {
+   private int getRandomDelay() {
+      final int MAX = 500;
+      final int MIN = (int) (MAX / 2.0);
+      final int RANGE = MAX - MIN + 1;
+      return (int) (Math.random() * RANGE + MIN);
+   }
 
-	// avoid unnecessary overhead when the list is small/empty
-	if (list.size() <= 1) {
-	    return ColorExtensions.getRandomDarkColor();
-	}
-	
-	boolean keepLooking = true;
-	// vary the minimum separation between colors of each ball colors based on the size of the list.
-	// The more items in the list, the less unique the colors need to be.
-	int byTotalRGB = (int) (64.0 / list.size());
-	//System.out.format("uniqueColor: list size = %d, minimum difference = %d%n", list.size(), byTotalRGB);
-	Color c = null;
-	int count = 0;
-	int attempts = 0;
-	// vary the maximum number of attempts to insure unique colors based on the size of the list.
-	// The more items in the list, the less unique the colors need to be.
-	int maxAttempts = (int) (2048.0 / list.size());
-	do {
-	    attempts++;
-	    c = ColorExtensions.getRandomDarkColor();
-	    // check the other balls to see if this new ball's color is unique enough
-	    for (RandomBall b : list) {		
-		// System.out.format("Color difference %d = %d%n", i,
-		// ColorExtensions.getColorDifference(c, b.getColor()));
-		if (ColorExtensions.areSignificantlyDifferentColors(c, b.getColor(), byTotalRGB)) {
-		    count++;
-		}
-	    }
-	    // stop if we have unique colors or have exceeded the maximum number of attempts to find a unique color
-	    if (count == list.size()) {
-		keepLooking = false;
-	    } else if (attempts == maxAttempts) {
-		System.out.format("gave up looking for unique color in list of size = %d after %d attempts%n", list.size(), attempts);
-		keepLooking = false;
-	    }
+   private double getRandomX() {
+      double MAX = gameSize.width;
+      double MIN = 0;
+      double RANGE = MAX - MIN + 1;
+   
+      return Math.random() * RANGE + MIN;
+   }
 
-	} while (keepLooking);
+   private double getRandomY() {
+      double MAX = gameSize.height;
+      double MIN = 0;
+      double RANGE = MAX - MIN + 1;
+   
+      return Math.random() * RANGE + MIN;
+   }
 
-	return c;
+   private double getRandomDiameter() {
+      double MAX = gameSize.height * 7.0 / 100.0;
+      double MIN = MAX * 60.0 / 100;
+      double RANGE = MAX - MIN + 1;
+   
+      return Math.random() * RANGE + MIN;
+   }
 
-    }
+   public ArrayList<RandomBall> getBalls() {
+      return balls;
+   }
 
-    private int getRandomNumberOfBalls() {
-	final int MAX = 9;
-	final int MIN = 3;
-	final int RANGE = MAX - MIN + 1;
-	return (int) (Math.random() * RANGE + MIN);
-    }
-
-    private int getRandomDelay() {
-	final int MAX = 500;
-	final int MIN = (int) (MAX / 2.0);
-	final int RANGE = MAX - MIN + 1;
-	return (int) (Math.random() * RANGE + MIN);
-    }
-
-    private double getRandomX() {
-	double MAX = gameSize.width;
-	double MIN = 0;
-	double RANGE = MAX - MIN + 1;
-
-	return Math.random() * RANGE + MIN;
-    }
-
-    private double getRandomY() {
-	double MAX = gameSize.height;
-	double MIN = 0;
-	double RANGE = MAX - MIN + 1;
-
-	return Math.random() * RANGE + MIN;
-    }
-
-    private double getRandomDiameter() {
-	double MAX = gameSize.height * 7.0 / 100.0;
-	double MIN = MAX * 60.0 / 100;
-	double RANGE = MAX - MIN + 1;
-
-	return Math.random() * RANGE + MIN;
-    }
-
-    public ArrayList<RandomBall> getBalls() {
-	return balls;
-    }
-
-    public Dimension getPanelSize() {
-	return this.gameSize;
-    }
+   public Dimension getPanelSize() {
+      return this.gameSize;
+   }
 }
